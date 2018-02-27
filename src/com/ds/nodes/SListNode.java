c369ae1fc3c4c0e07aec602b888422df3c5399d8
varpen01 <varpen01@roberthalf.com>
2018-02-26 16:34:13 -0800

    Added Singly linked list node

---
+++ src/com/ds/nodes/SListNode.java
@@ -0,0 +1,12 @@
+package com.ds.nodes;
+
+public class SListNode {
+
+    public SListNode next;
+    public Integer data;
+
+    public SListNode(Integer data){
+        this.data=data;
+        this.next=null;
+    }
+}
