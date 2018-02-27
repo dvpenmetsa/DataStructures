86e898366d6fd987e3ab75017c3b0f3134f63921
varpen01 <varpen01@roberthalf.com>
2018-02-27 14:19:18 -0800

    Added DeleteNode

--- src/learn/ds/LinkedList.java
+++ src/learn/ds/LinkedList.java
@@ -7,16 +7,16 @@ public class LinkedList {
     SListNode head;
 
     /* Push adds node on front of the list*/
-    public void push(int data){
+    public void push(int data) {
         SListNode new_node = new SListNode(data);
-        new_node.next=head;
-        head=new_node;
+        new_node.next = head;
+        head = new_node;
 
     }
 
     /* Pop removes first element in the linked list*/
-    public void pop(){
-        if(head == null){
+    public void pop() {
+        if (head == null) {
             System.out.println("\nNo Elements to pop ");
             return;
         }
@@ -24,81 +24,105 @@ public class LinkedList {
     }
 
     /* Print elements of a linked list*/
-    public void print(SListNode head){
-        while(head != null){
-            System.out.print(head.data+ " ");
-            head=head.next;
+    public void print(SListNode head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
         }
     }
 
     /* Insert after a given node */
-    public void insertAfter(SListNode prev, int new_data){
+    public void insertAfter(SListNode prev, int new_data) {
 
         SListNode new_node = new SListNode(new_data);
         SListNode temp;
 
-        if (null==prev){
+        if (null == prev) {
             System.out.println("Given node cant't be null");
             return;
         }
         new_node.next = prev.next;
-        prev.next=new_node;
+        prev.next = new_node;
     }
 
     /* Add after last node */
-    public void append(int new_data){
+    public void append(int new_data) {
 
-      if(head == null){
-         head =  new SListNode(new_data);
-         return;
-      }
-      SListNode new_node = new SListNode(new_data);
-      SListNode node = head;
-      while (node.next!=null){
-          node=node.next;
-      }
-      node.next=new_node;
+        if (head == null) {
+            head = new SListNode(new_data);
+            return;
+        }
+        SListNode new_node = new SListNode(new_data);
+        SListNode node = head;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = new_node;
     }
+
     /* Given a ‘key’, delete the first occurrence of this key in linked list. */
-    public void deleteKey(int new_data){
+    public void deleteKey(int new_data) {
         SListNode node = head;
         SListNode prevNode = null;
 
-        if(node != null && node.data==new_data){
-           head=node.next;
-           return;
+        if (node != null && node.data == new_data) {
+            head = node.next;
+            return;
         }
 
-        while(node !=null && node.data!=new_data){
+        while (node != null && node.data != new_data) {
             prevNode = node;
-            node=node.next;
+            node = node.next;
         }
 
-        if (node == null){
+        if (node == null) {
             System.out.println("Key not found.");
             return;
         }
-        prevNode.next=node.next;
+        prevNode.next = node.next;
+    }
+
+    /* Given a reference (pointer to pointer) to the head of a list
+       and a position, deletes the node at the given position */
+    public void deleteNode(SListNode node) {
+       SListNode temp =head;
+       SListNode prev = null;
+
+       if(temp == node){
+           head=temp.next;
+           return;
+       }
+       while(temp != null && temp != node){
+           prev = temp;
+           temp=temp.next;
+       }
+
+       if (temp == null){
+           System.out.println("Give node not found");
+           return;
+       }
+       prev.next=temp.next;
     }
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         SListNode a = new SListNode(20);
         SListNode b = new SListNode(30);
-        ll.head= new SListNode(10);
+        ll.head = new SListNode(10);
 
         ll.head.next = a;
-        ll.head.next.next=b;
+        ll.head.next.next = b;
 
         ll.push(5);
         ll.push(1);
 
-        ll.insertAfter(a,25);
+        ll.insertAfter(a, 25);
         ll.append(70);
         ll.print(ll.head);
         System.out.println("\n");
         ll.deleteKey(15);
+        ll.deleteNode(ll.head);
         ll.print(ll.head);
     }
 
