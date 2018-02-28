7c0e582ed93ef19c7aaf607859a98aa2a3e53a9a
varpen01 <varpen01@roberthalf.com>
2018-02-27 16:47:54 -0800

    Added swap nodes

--- src/learn/ds/LinkedList.java
+++ src/learn/ds/LinkedList.java
@@ -162,29 +162,99 @@ public class LinkedList {
        return searchRecursive(head.next,data);
     }
 
+    /* Swap nodes in a linked list without swapping data */
+    public void swapNode(SListNode x, SListNode y) {
+
+        //Search prev of node x
+        SListNode currX = head;
+        SListNode prevX = null;
+
+        if (currX == null) return;
+        while (currX != null && currX != x) {
+            prevX = currX;
+            currX = currX.next;
+        }
+        if (currX == null) return;
+
+        //Search prev of node Y
+        SListNode currY = head;
+        SListNode prevY = null;
+
+        if (currY == null) return;
+        while (currY != null && currY != y) {
+            prevY = currY;
+            currY = currY.next;
+        }
+        if (currY == null) return;
+
+        //If x is head
+        if(prevX == null){
+            SListNode temp = currX.next;
+            currX.next = currY.next;
+            currY.next = temp;
+
+            prevY.next=currX;
+            head=currY;
+            return;
+        }
+
+
+        //If Y is head
+        if(prevY == null){
+            SListNode temp = currY.next;
+            currY.next = currX.next;
+            currX.next = temp;
+
+            prevX.next=currY;
+            head=currX;
+            return;
+        }
+
+        //Swapping nodes
+        SListNode temp = currX.next;
+        currX.next = currY.next;
+        currY.next= temp;
+
+        prevX.next=currY;
+        prevY.next=currX;
+
+    }
+
 
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         SListNode a = new SListNode(20);
         SListNode b = new SListNode(30);
+        SListNode c = new SListNode(40);
+        SListNode d = new SListNode(50);
+        SListNode e = new SListNode(60);
+        SListNode f = new SListNode(70);
         ll.head = new SListNode(10);
 
         ll.head.next = a;
         ll.head.next.next = b;
+        ll.head.next.next.next = c;
+        ll.head.next.next.next.next = d;
+        ll.head.next.next.next.next.next = e;
+        ll.head.next.next.next.next.next.next = f;
 
-        ll.push(5);
-        ll.push(1);
+        //ll.push(5);
+        //ll.push(1);
 
         ll.insertAfter(a, 25);
-        ll.append(70);
-        System.out.println("Length of linked list: " + ll.length());
-        /*ll.print(ll.head);
-        System.out.println("\n");
+        ll.append(80);
+
+        /*System.out.println("Length of linked list: " + ll.length());
         ll.deleteKey(15);
         ll.deleteNode(ll.head);
         ll.deleteAfterPosition(7);
-        ll.print(ll.head);*/
-        System.out.println(ll.searchRecursive(ll.head,10));
+        ll.print(ll.head);
+        System.out.println(ll.searchRecursive(ll.head,10));*/
+
+        ll.print(ll.head);
+        System.out.println("\n");
+        ll.swapNode(a,d);
+        ll.print(ll.head);
     }
 
 }
