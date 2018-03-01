bf64f661396babe1bec45f5a2622aa0a7d312313
Varma <dvpenmetsa@gmail.com>
2018-02-28 20:56:51 -0800

    Removed swap node from linked list

--- src/learn/ds/linkedlist/LinkedList.java
+++ src/learn/ds/linkedlist/LinkedList.java
@@ -119,62 +119,6 @@ public class LinkedList {
         return node;
     }
 
-    public void swapNodes(SListNode x, SListNode y) {
-
-        //Search prev of node x
-        SListNode currX = head;
-        SListNode prevX = null;
-
-        if (currX == null) return;
-        while (currX != null && currX != x) {
-            prevX = currX;
-            currX = currX.next;
-        }
-        if (currX == null) return;
-
-        //Search prev of node Y
-        SListNode currY = head;
-        SListNode prevY = null;
-
-        if (currY == null) return;
-        while (currY != null && currY != y) {
-            prevY = currY;
-            currY = currY.next;
-        }
-        if (currY == null) return;
-
-        //If x is head
-        if(prevX == null){
-            SListNode temp = currX.next;
-            currX.next = currY.next;
-            currY.next = temp;
-
-            prevY.next=currX;
-            head=currY;
-            return;
-        }
-
-        //If Y is head
-        if(prevY == null){
-            SListNode temp = currY.next;
-            currY.next = currX.next;
-            currX.next = temp;
-
-            prevX.next=currY;
-            head=currX;
-            return;
-        }
-
-        //Swapping nodes
-        SListNode temp = currX.next;
-        currX.next = currY.next;
-        currY.next= temp;
-
-        prevX.next=currY;
-        prevY.next=currX;
-
-    }
-
     public static void main(String args[]) {
         LinkedList ll = new LinkedList(new SListNode(10));
         SListNode a = new SListNode(20);
@@ -203,7 +147,6 @@ public class LinkedList {
         System.out.println("Length of linked list: " + ll.length(ll.head));
         System.out.println(ll.searchRecursive(ll.head,10));
         System.out.println(ll.getNode(9));
-        ll.swapNodes(ll.head,f);
         ll.display();
     }
 
