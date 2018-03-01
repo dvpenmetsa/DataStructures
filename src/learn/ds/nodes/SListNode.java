472810df561c2f5d8e28f45f0ac852f7ad580246
Varma <dvpenmetsa@gmail.com>
2018-02-28 19:52:56 -0800

    Refactured code

---
+++ src/learn/ds/nodes/SListNode.java
@@ -0,0 +1,15 @@
+package learn.ds.nodes;
+
+public class SListNode {
+
+    /*
+      Basic singly linked list node
+     */
+    public SListNode next;
+    public int data;
+
+    public SListNode(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
