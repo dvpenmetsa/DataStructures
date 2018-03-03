a66b6d904a6d1d2541a8b9cd78a1fb4cd81764ca
Varma <dvpenmetsa@gmail.com>
2018-03-02 20:37:54 -0800

    Added class to add check palindrome

---
+++ src/learn/ds/linkedlist/IsPalindrome.java
@@ -0,0 +1,20 @@
+package learn.ds.linkedlist;
+
+import learn.ds.nodes.SListNode;
+
+import java.util.Stack;
+
+/**
+ * @author Varma Penmetsa
+ */
+public class IsPalindrome {
+
+    /* Check if give linked list is palindrome, using stacks */
+    public static void palindromeCheck(SListNode head){
+
+    }
+
+    public static void main(String args[]){
+
+    }
+}
