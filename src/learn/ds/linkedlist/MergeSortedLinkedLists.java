eb5db0d2a45f0c51a9c3e0db97713721f4ce6d43
Varma <dvpenmetsa@gmail.com>
2018-03-02 17:39:46 -0800

    Added methods to merge sorted linked list

--- src/learn/ds/linkedlist/MergeSortedLinkedLists.java
+++ src/learn/ds/linkedlist/MergeSortedLinkedLists.java
@@ -32,6 +32,74 @@ public class MergeSortedLinkedLists {
         return head.next;
     }
 
+    /* Merge sorted linked list with out dummy node */
+    public static SListNode sortedMerge2(SListNode l1, SListNode l2){
+        SListNode head=null;
+        SListNode ret;
+
+        if(l1==null && l2==null){
+            return head;
+        }else if(l1==null && l2!=null){
+            return l2;
+        }else if(l1!=null && l2==null){
+            return l1;
+        }
+
+        if(l1.data < l2.data){
+            head=l1;
+            l1=l1.next;
+        }else if(l1.data > l2.data){
+            head = l2;
+            l2=l2.next;
+        }
+
+        ret = head;
+
+        while(l1!=null && l2!=null){
+            if(l1.data<l2.data){
+                head.next=l1;
+                l1=l1.next;
+            }else if(l1.data>l2.data){
+                head.next=l2;
+                l2=l2.next;
+            }
+            head=head.next;
+        }
+
+        if(l1!=null){
+            head.next=l1;
+        }
+
+        if(l2!=null){
+            head.next=l2;
+        }
+
+        return ret;
+    }
+
+    /* Merge sorted linked list using recursion*/
+    public static SListNode sortedMerge3(SListNode l1, SListNode l2){
+        SListNode temp = null;
+
+        if(l1==null && l2==null){
+            return temp;
+        }else if(l1!=null && l2==null){
+            return l1;
+        }else if(l1==null && l2!=null){
+            return l2;
+        }
+
+        if (l1.data<l2.data){
+            temp = l1;
+            l1=l1.next;
+        }else if(l1.data>l2.data){
+            temp = l2;
+            l2=l2.next;
+        }
+        temp.next = sortedMerge3(l1,l2);
+        return temp;
+    }
+
     public static void main(String args[]) {
         LinkedList list1 = new LinkedList(new SListNode(10));
         list1.append(30);
@@ -53,8 +121,9 @@ public class MergeSortedLinkedLists {
         list1.display();
         list2.display();
 
-        list1.display2(sortedMerge(list1.head,list2.head));
-
+        list1.display2(sortedMerge(list1.head,null));
+        list1.display2(sortedMerge2(null,list2.head));
+        list1.display2(sortedMerge3(list1.head,list2.head));
 
     }
 }
