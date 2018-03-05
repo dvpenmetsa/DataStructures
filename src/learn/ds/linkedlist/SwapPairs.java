279870cde52a8bb796d5f383f38b895d1a898edb
varpen01 <varpen01@roberthalf.com>
2018-03-05 14:46:09 -0800

    Updated swap pairs

--- src/learn/ds/linkedlist/SwapPairs.java
+++ src/learn/ds/linkedlist/SwapPairs.java
@@ -1,20 +1,24 @@
 package learn.ds.linkedlist;
 
 import learn.ds.nodes.SListNode;
+import learn.ds.util.HelperLinkedList;
 import learn.ds.util.RandLinkedListGenetator;
 
 /**
  * @author Varma Penmetsa
  *
- * Given a linked list, swap every two adjacent nodes and return its head.
+ * Given a linked list, swap every two adjacent nodes(Key is to swap values instead of nodes)
  * For example, given 1->2->3->4, you should return the list as 2->1->4->3.
- * Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
  *
  */
 public class SwapPairs {
 
-    // This method chages data in the pointers
-    public static void swap3(SListNode head) {
+    /*
+     * Swap node using iteration
+     * Time complexity : O(n)
+     * Space complexity : O(1)
+     */
+    public static void swap1(SListNode head) {
         int i = 0;
         SListNode curr = head;
         SListNode prev = null;
@@ -31,39 +35,29 @@ public class SwapPairs {
             }
             curr = curr.next;
         }
-
     }
 
-    public static int swap2(SListNode head) {
-       if(head==null){
-           return 0;
+    /*
+     * Swap node using recursion
+     * Time complexity : O(n)
+     * Space complexity : O(1)
+     */
+    public static void swap2(SListNode head) {
+       if(head==null || head.next ==null){
+           return;
        }
-       int count = 1 + swap2(head.next);
-       if(count%2==0){
-           int temp = head.data;
-           head.data=head.next.data;
-           head.next.data=temp;
-       }
-       return count;
-    }
-
-    public static void swap(SListNode head){
-       SListNode prev = null;
-       SListNode curr = head;
-
-       while(curr!=null){
-
-       }
+       int temp = head.data;
+       head.data=head.next.data;
+       head.next.data=temp;
+       swap2(head.next.next);
+       return;
     }
 
     public static void main(String args[]) {
         RandLinkedListGenetator rg = new RandLinkedListGenetator();
-        LinkedList ll = rg.create(7);
+        LinkedList ll = rg.create(1);
         ll.display();
         swap2(ll.head);
         ll.display();
-
     }
-
-
 }
