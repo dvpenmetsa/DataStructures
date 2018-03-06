fc2fa0912aa8626ebd888bb41e41d5f651b6da71
Varma <dvpenmetsa@gmail.com>
2018-03-05 20:04:23 -0800

    ToDo Merge Sort

---
+++ src/learn/ds/linkedlist/MergeSort.java
@@ -0,0 +1,23 @@
+package learn.ds.linkedlist;
+
+/**
+ * @author Varma Penmetsa
+ *
+ * https://www.geeksforgeeks.org/merge-sort-for-linked-list/
+ */
+public class MergeSort {
+
+    /*
+     * MergeSort(headRef)
+     * 1) If head is NULL or there is only one element in the Linked List then return.
+     * 2) Else divide the linked list into two halves.
+     *    FrontBackSplit(head, &a, &b); /* a and b are two halves
+     * 3) Sort the two halves a and b.
+     *     MergeSort(a);
+     *     MergeSort(b);
+     * 4) Merge the sorted a and b using SortedMerge() and update the head pointer using headRef.
+     *     headRef = SortedMerge(a, b);
+     */
+
+
+}
