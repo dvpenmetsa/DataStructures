3b3b18e8ec82e8123a31bc423c3cb52b39a5ef62
varpen01 <varpen01@roberthalf.com>
2018-03-09 17:00:05 -0800

    ToDo LinkedList Union and Intersection

---
+++ src/learn/ds/linkedlist/UnionAndIntersection.java
@@ -0,0 +1,23 @@
+package learn.ds.linkedlist;
+
+import learn.ds.nodes.SListNode;
+
+/**
+ * @author Varma Penmetsa
+ *
+ *  Create union and intersection lists that contain union & intersection of the elements present in the given lists.
+ *  Order of elments in output lists doesn’t matter.
+ *
+ *  Input:
+ *     List1: 10->15->4->20
+ *     List2:  8->4->2->10
+ *  Output:
+ *     Intersection List: 4->10
+ *     Union List: 2->8->20->4->15->10
+ *
+ * https://www.geeksforgeeks.org/union-and-intersection-of-two-linked-lists/
+ *
+ */
+public class UnionAndIntersection {
+    //ToDo
+}
