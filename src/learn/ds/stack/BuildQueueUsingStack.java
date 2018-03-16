5b0d7ec117631098f5939175d60af5f56952ee8a
varpen01 <varpen01@roberthalf.com>
2018-03-16 15:02:24 -0700

    Implement Queue using Stacks

---
+++ src/learn/ds/stack/BuildQueueUsingStack.java
@@ -0,0 +1,41 @@
+package learn.ds.stack;
+
+import learn.ds.util.Stack;
+
+/**
+ * @author Varma Penmetsa
+ *
+ * https://www.geeksforgeeks.org/queue-using-stacks/
+ */
+
+public class BuildQueueUsingStack {
+
+    Stack tStack  = new Stack();
+    Stack qStack  = new Stack();
+
+    public void enQueue(String val){
+        tStack.push(val);
+    }
+
+    public String deQueue(){
+        while(!tStack.isEmpty()){
+            qStack.push(tStack.pop());
+        }
+        return qStack.pop();
+    }
+
+    public static void main(String[] args){
+        BuildQueueUsingStack q = new BuildQueueUsingStack();
+        q.enQueue("S");
+        q.enQueue("T");
+        q.enQueue("A");
+        q.enQueue("C");
+        q.enQueue("K");
+
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+        System.out.println(q.deQueue());
+    }
+}
