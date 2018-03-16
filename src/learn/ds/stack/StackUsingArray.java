e4384b29787998f3a854f5c073c75bb5a1f2ca76
varpen01 <varpen01@roberthalf.com>
2018-03-16 14:03:53 -0700

    Stack using array

---
+++ src/learn/ds/stack/StackUsingArray.java
@@ -0,0 +1,72 @@
+package learn.ds.stack;
+
+/**
+ * @author Varma Penmetsa
+ *
+ * https://www.geeksforgeeks.org/stack-data-structure-introduction-program/
+ */
+
+public class StackUsingArray {
+    public int stackSize;
+    public String[] stackArray;
+    public int top = 0;
+
+    public StackUsingArray(int stackSize) {
+        this.stackSize = stackSize;
+        stackArray = new String[stackSize];
+    }
+
+    public void push(String val) {
+        if (top < stackSize) {
+            stackArray[top++] = val;
+        } else {
+            System.out.println("Stack Overflow");
+        }
+    }
+
+    public String pop() {
+        if (top == 0) {
+            System.out.println("Stack Underflow");
+            return null;
+        } else {
+            String ret = stackArray[--top];
+            stackArray[top] = null;
+            return ret;
+        }
+    }
+
+    public String peek() {
+        if (top < stackSize) {
+            return stackArray[top - 1];
+        } else {
+            System.out.println("Stack Underflow");
+            return "";
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String args[]) {
+
+        StackUsingArray st = new StackUsingArray(5);
+        st.push("S");
+        st.push("T");
+        st.push("A");
+        st.push("C");
+        st.push("K");
+
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+        System.out.println(st.pop());
+
+        System.out.println("Is stack empty: " + st.isEmpty());
+    }
+}
