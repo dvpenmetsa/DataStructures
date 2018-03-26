54a05b30d9609e27854c3d793d11ec53d9f3aac7
Varma <dvpenmetsa@gmail.com>
2018-03-26 08:55:34 -0700

    ToDo iterative approach

--- src/learn/ds/stack/TowerOfHanoi.java
+++ src/learn/ds/stack/TowerOfHanoi.java
@@ -5,7 +5,7 @@ import java.util.Deque;
 
 /**
  * @author Varma Penmetsa
- *
+ * <p>
  * https://www.geeksforgeeks.org/iterative-tower-of-hanoi/
  */
 public class TowerOfHanoi {
@@ -21,8 +21,79 @@ public class TowerOfHanoi {
         moveTower(rings - 1, auxTower, toTower, fromTower);
     }
 
+    //Iterative Approach using stacks
+    public static Deque<Integer> moveTower2(int num_of_disks, Deque<Integer> fromTower, Deque<Integer> toTower, Deque<Integer> auxTower) {
+
+        char s = 'S', d = 'D', a = 'A';
+
+        // If number of disks is even, then interchange
+        // destination pole and auxiliary pole
+        if (num_of_disks % 2 == 0) {
+            char temp = d;
+            d = a;
+            a = temp;
+        }
+        int totalMoves = (int) (Math.pow(2, num_of_disks) - 1);
+
+        for (int i = num_of_disks; i >= 1; i--) {
+            fromTower.push(i);
+        }
+
+        for (int i = 1; i <= totalMoves; i++) {
+            if (i % 3 == 1) {
+                moveDisksBetweenTwoPoles(fromTower, toTower, s, d);
+            } else if (i % 3 == 2) {
+                moveDisksBetweenTwoPoles(fromTower, auxTower, s, a);
+            } else if (i % 3 == 0) {
+                moveDisksBetweenTwoPoles(auxTower, toTower, a, d);
+            }
+        }
+        return toTower;
+    }
+
+    public static void moveDisksBetweenTwoPoles(Deque<Integer> src, Deque<Integer> dest, char s, char d) {
+        int pole1TopDisk = src.pop();
+        int pole2TopDisk = dest.pop();
+
+        // When pole 1 is empty
+        if (pole1TopDisk == Integer.MIN_VALUE) {
+            src.push(pole2TopDisk);
+            moveDisk(d, s, pole2TopDisk);
+        }
+        // When pole2 pole is empty
+        else if (pole2TopDisk == Integer.MIN_VALUE) {
+            dest.push(pole1TopDisk);
+            moveDisk(s, d, pole1TopDisk);
+        }
+        // When top disk of pole1 > top disk of pole2
+        else if (pole1TopDisk > pole2TopDisk) {
+            src.push(pole1TopDisk);
+            src.push(pole2TopDisk);
+            moveDisk(d, s, pole2TopDisk);
+        }
+        // When top disk of pole1 < top disk of pole2
+        else {
+            dest.push(pole2TopDisk);
+            moveDisk(s, d, pole1TopDisk);
+        }
+    }
+
+    static void moveDisk(char fromPeg, char toPeg, int disk) {
+        System.out.println("Move the disk " + disk + " from " + fromPeg + " to " + toPeg);
+    }
+
     public static void main(String[] args) {
-        moveTower(2,'1','2','3');
+        moveTower(6,'1','2','3');
+
+//        Deque<Integer> fromTower = new ArrayDeque<>();
+//        Deque<Integer> toTower = new ArrayDeque<>();
+//        Deque<Integer> auxTower = new ArrayDeque<>();
+//
+//        moveTower2(3, fromTower, toTower, auxTower);
+//
+//        for (int ele : toTower) {
+//            System.out.println(ele);
+//        }
     }
 
 }
