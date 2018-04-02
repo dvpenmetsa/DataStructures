5b742ac22eced5daa999873be6df28ef266b38a9
varpen01 <varpen01@roberthalf.com>
2018-04-02 15:39:41 -0700

    Added util methods

---
+++ src/learn/ds/util/ArrayUtil.java
@@ -0,0 +1,11 @@
+package learn.ds.util;
+
+public class ArrayUtil {
+
+    public static void display(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
