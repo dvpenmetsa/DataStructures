0aa3fa2528f43b988db4c41fd58b7c1bca17e28b
Varma <dvpenmetsa@gmail.com>
2018-04-11 08:02:43 -0700

    Added Recursive merge sort

---
+++ src/learn/algo/sort/MergeSort.java
@@ -0,0 +1,68 @@
+package learn.algo.sort;
+
+import learn.ds.util.ArrayUtil;
+
+/**
+ * @author Varma Penmetsa
+ * https://www.youtube.com/watch?v=TzeBrDU-JaY
+ * https://github.com/mission-peace/interview/blob/68f97392c6f6e14e2c08860684ed605a1a5e7e72/src/com/interview/sort/MergeSort.java
+ */
+public class MergeSort {
+
+    /**
+     * Time Complexity: O (n log n)
+     * Space Complexity: O (n)
+     */
+    public static void sort(int[] array){
+        sort(array, 0, array.length-1);
+    }
+
+    public static void sort(int[] array, int left, int right){
+        if (left >= right){
+            return;
+        }
+
+        int mid = (left + right) / 2;
+        sort(array, left, mid);
+        sort(array,mid+1, right);
+        sortedHalves(array,left,right);
+    }
+
+    public static void sortedHalves(int[] array, int left, int right){
+        int mid = (left + right) / 2;
+        int[] temp = new int[right - left + 1];
+
+        // i is start of left, j is start of right and r is start of temp
+        int i = left;
+        int j = mid + 1;
+        int r = 0;
+
+        while(i <= mid && j <= right){
+            if(array[i] < array[j]){
+                temp[r++] = array[i++];
+            }else {
+                temp[r++] = array[j++];
+            }
+        }
+
+        while(i <= mid){
+            temp[r++] = array[i++];
+        }
+
+        while (j <= right){
+            temp[r++] = array[j++];
+        }
+
+        //Copying it back to array
+        i = left;
+        for (int k = 0; k < temp.length; ) {
+            array[i++] = temp[k++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3, 2, 8, 5, 9, 4, 1, 6};
+        sort(array);
+        ArrayUtil.display(array);
+    }
+}
