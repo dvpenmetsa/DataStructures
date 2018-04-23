be641fa1ec07eb7b0c0a49e2e77b6104b3aea7c9
varpen01 <varpen01@roberthalf.com>
2018-04-23 10:13:14 -0700

    Rearrange positive and negative numbers with constant extra space

--- src/learn/ds/array/Rearrange.java
+++ src/learn/ds/array/Rearrange.java
@@ -5,7 +5,7 @@ import learn.util.ArrayUtil;
 /**
  * @author Varma Penmetsa
  *
- * iven an array of positive and negative numbers, arrange them such that all negative integers
+ * Given an array of positive and negative numbers, arrange them such that all negative integers
  * appear before all the positive integers in the array without using any additional data structure like hash table,
  * arrays, etc. The order of appearance should be maintained.
  *
@@ -13,28 +13,62 @@ import learn.util.ArrayUtil;
  */
 public class Rearrange {
 
-    public static void arrage(int[] array){
-        if(array.length == 0){
+    //Optimize Merge sort
+    public static void arrange(int[] array) {
+        arrange(array, 0, array.length - 1);
+    }
+
+    public static void arrange(int[] array, int left, int right){
+        if(left >= right){
             return;
         }
-        int low = 0;
-        int high = array.length -1;
+        int mid  = left + (right - left ) /2;
+        arrange(array,left, mid);
+        arrange(array,mid+1, right);
+        mergeHalfs(array, mid, left, right);
+    }
 
-        while(low <= high){
+    public static void mergeHalfs(int[] array, int mid, int left, int right){
+        int leftStart = left;
+        int rightStart =  mid + 1;
 
-            if(array[low] < 0){
-                low++;
-            }else if(array[high] >= 0){
-                high--;
-            }else {
-                ArrayUtil.swap(array,low++,high--);
-            }
+        int lSize = mid + 1 - left;
+        int rSize = right - mid;
+
+        int L[] = new int[lSize];
+        int R[] = new int[rSize];
+        int i , j ,k;
+
+        for (i = 0; i < lSize; i++) {
+            L[i] = array[leftStart + i];
         }
+        for(j = 0 ; j < rSize; j++){
+            R[j] = array[rightStart + j];
+        }
+
+        i = 0; j = 0; k = leftStart;
+
+        while(i < lSize && L[i] < 0){
+            array[k++] = L[i++];
+        }
+
+        while(j < rSize && R[j] < 0){
+            array[k++] = R[j++];
+        }
+
+        while(i < lSize){
+            array[k++] = L[i++];
+        }
+
+        while (j < rSize){
+            array[k++] = R[j++];
+        }
+
     }
 
     public static void main(String[] args) {
         int[] array = {12, 11, -13, -5, 6, -7, 5, -3, -6};
-        arrage(array);
+        arrange(array);
         ArrayUtil.display(array);
     }
 }
