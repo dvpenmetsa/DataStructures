41f85bb9e3e2d9d0f9d4698dec10ce3d6740c381
Varma <dvpenmetsa@gmail.com>
2018-05-22 08:12:52 -0700

    Added postorder traversal with two stacks

---
+++ src/learn/ds/tree/PostOrderIterative.java
@@ -0,0 +1,51 @@
+package learn.ds.tree;
+
+import learn.ds.nodes.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * @author Varma Penmetsa
+ */
+public class PostOrderIterative {
+
+    /**
+     * Using two stacks
+     *
+     * Time Complexity  : O(n)
+     * Space Complexity : O(n)
+     */
+    public static void postOrder1(TreeNode root){
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+
+        st1.push(root);
+
+        while(!st1.isEmpty()){
+            TreeNode node  = st1.pop();
+            st2.push(node);
+
+            if(node.left !=null){
+                st1.push(node.left);
+            }
+            if(node.right !=null){
+                st1.push(node.right);
+            }
+        }
+
+        while(!st2.isEmpty()){
+            System.out.print(st2.pop().data + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        postOrder1(root);
+    }
+}
