8dd1d3025039e8041344218418fa50c048c7d5f3
varpen01 <varpen01@roberthalf.com>
2018-08-13 13:44:32 -0700

    Added new problems

---
+++ src/learn/algo/math/CheckPrimeNumber.java
@@ -0,0 +1,36 @@
+package learn.algo.math;
+
+/**
+ * How we check whether a number is Prime or not?
+ *
+ * https://www.geeksforgeeks.org/prime-numbers/
+ */
+
+public class CheckPrimeNumber {
+
+    /*Brute force
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static void check(int number){
+        if(number <= 1){
+            System.out.println("False");
+            return;
+        }
+
+        int n = number - 1;
+        while(n > 1){
+            if(number % n == 0){
+                System.out.println("False");
+                return;
+            }
+            n--;
+        }
+        System.out.println("True");
+    }
+
+    public static void main(String[] args) {
+        check(11);
+    }
+
+}
