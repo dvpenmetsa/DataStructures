472182121238223f7e3523ae87bd13460c43484c
Varma <dvpenmetsa@gmail.com>
2018-08-14 09:21:55 -0700

    Added new problems

---
+++ src/learn/ds/string/easy/ReverseString.java
@@ -0,0 +1,45 @@
+package learn.ds.string.easy;
+
+/**
+ * @author Varma Penmetsa
+ *
+ * Write a function that takes a string as input and returns the string reversed.
+ *
+ * Example 1:
+ *    Input: "hello"
+ *    Output: "olleh"
+ *
+ *Example 2:
+ *    Input: "A man, a plan, a canal: Panama"
+ *    Output: "amanaP :lanac a ,nalp a ,nam A"
+ *
+ * https://leetcode.com/problems/reverse-string/description/
+ */
+public class ReverseString {
+
+    /**
+     * Time Complexity  : O(n)
+     * Space Complexity : O(1)
+     */
+
+    public static String reverseString(String s) {
+        if(s == "" || s.length() == 0 || s.length() == 1){
+            return s;
+        }
+
+        char[] c = s.toCharArray();
+        int n = c.length-1;
+        System.out.println(n/2);
+
+        for (int i=0; i <= n/2 ; i++){
+            char temp = c[i];
+            c[i]=c[n-i];
+            c[n-i]=temp;
+        }
+        return new String(c);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("A mana"));
+    }// anma A
+}
---
+++ src/learn/ds/string/easy/UniqueMooseCodeWords.java
@@ -0,0 +1,52 @@
+package learn.ds.string.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Varma Penmetsa
+ *
+ * nternational Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+ *
+ * For convenience, the full table for the 26 letters of the English alphabet is given below:
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+ *
+ * Return the number of different transformations among all words we have.
+ *
+ * https://leetcode.com/problems/unique-morse-code-words/description/
+ */
+public class UniqueMooseCodeWords {
+
+    /**
+     * Time Complexity  : O(n ^ 2)
+     * Space Complexity : O(n)
+     */
+
+    public static int uniqueMooseRepresentation(String[] words){
+        if(words.length == 0){
+            return 0;
+        }else if(words.length == 1){
+            return 1;
+        }
+
+        String[] mooseCodes = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> uSet = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(String word : words){
+            sb.setLength(0);
+            for(char c : word.toCharArray()){
+                sb.append(mooseCodes[c - 97]);
+            }
+            uSet.add(sb.toString());
+        }
+        return uSet.size();
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"gin", "zen", "gig", "msg"};
+        System.out.println(uniqueMooseRepresentation(words));
+    }
+}
