aa72e0a4bb91ad73fac32fb16b661f424dad8261
Varma <dvpenmetsa@gmail.com>
2018-09-03 22:11:45 -0700

    Added new problems

--- src/learn/ds/string/GroupAnagramsTogether.java
+++ src/learn/ds/string/GroupAnagramsTogether.java
@@ -1,5 +1,7 @@
 package learn.ds.string;
 
+import java.util.*;
+
 /**
  * 9/3/18
  * @author Varma Penmetsa
@@ -19,4 +21,26 @@ package learn.ds.string;
  * https://leetcode.com/problems/group-anagrams/solution/
  */
 public class GroupAnagramsTogether {
+
+    public static List<List<String>> getAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList();
+        }
+        Map<String, List> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(getAnagrams(arr).toString());
+    }
 }
