54cabbbaaaf3b54be45ff17babe59e6d1b6e5d33
dvpenmetsa <dvpenmetsa@gmail.com>
2020-04-11 13:18:24 -0700

    Added new problem

---
+++ src/learn/ds/array/MoveZeros.java
@@ -0,0 +1,32 @@
+package learn.ds.array;
+
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Example:
+ *
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ *
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ *
+ * https://leetcode.com/problems/move-zeroes/
+ */
+public class MoveZeros {
+
+    public static void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
+                nums[i++] = nums[j];
+            }
+            j++;
+        }
+        while (i < nums.length) {
+            nums[i++] = 0;
+        }
+    }
+
+}
