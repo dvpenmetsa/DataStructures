d42264ad8e47db4055f861fe1ddac89d9343ec8b
dvpenmetsa <dvpenmetsa@gmail.com>
2020-05-07 10:53:01 -0700

    Added problem

--- src/learn/ds/matrix/RotateMatrixElements.java
+++ src/learn/ds/matrix/RotateMatrixElements.java
@@ -33,8 +33,53 @@ package learn.ds.matrix;
  *  13   11   7    8
  *  14   15   16   12
  *
+ * 
+ * 
  * https://www.geeksforgeeks.org/rotate-matrix-elements/
  *
  */
 public class RotateMatrixElements {
+
+    /**
+     * Transpose and then reverse:
+     * The obvious idea would be to transpose the matrix first and then reverse each row. 
+     * This simple approach already demonstrates the best possible time complexity
+     * @param matrix
+     */
+    public static void rotate(int[][] matrix){
+        int n = matrix.length;
+        //Step1: Transpose Matric
+        for(int i=0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
+                int temp = matrix[j][i];
+                matrix[j][i] =  matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        //Step 2: Swap last node to first
+        for(int i = 0 ; i < n; i++){
+            for(int j  = 0; j < n/2 ; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) { 
+            for (int j = 0; j < matrix.length; j++) 
+            System.out.print( matrix[i][j] + " "); 
+            System.out.print("\n"); 
+        } 
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { {1, 2, 3}, {4, 5, 6}, {7,8,9}}; 
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println("---------------------");
+        printMatrix(matrix);
+    }
 }
